54c0eb18f74eb6cb5ba941ad4698730654e15d46
prasanta1987 <prasanta.1987@hotmail.com>
2020-09-07 01:58:39 +0530

    updated

--- public/assets/tradebook.js
+++ public/assets/tradebook.js
@@ -2,24 +2,28 @@ const tradeDataMarkup = document.querySelector('.tradedata')
 
 const getTransactions = () => {
 
-    let transacrtions = userData.transactions.sort()
-    transacrtions.map(buyData => {
+    let boughtSymbols = []
+    let soldSymbols = []
+
+    userData.transactions.map(data => {
+        if (data.type == 'BUY' && !boughtSymbols.includes(data.symbol)) boughtSymbols.push(data.symbol)
+        if (data.type == 'SELL' && !soldSymbols.includes(data.symbol)) soldSymbols.push(data.symbol)
         tradeDataMarkup.innerHTML +=
             `
                 <tbody>
                     <tr class="text-center">
-                        <th scope="row" class="font-weight-normal">${buyData.symbol}</th>
-                        <td>${buyData.date}</td>
-                        <td>${buyData.price.toFixed(2)}</td>
-                        <td>${buyData.qty}</td>
-                        <td><kbd class="bg-light ${(buyData.type == 'BUY' ? 'text-danger' : 'text-success')}">${buyData.type}</kbd></td>
-                        <td>${(buyData.qty * buyData.price).toFixed(2)}</td>
+                        <th scope="row" class="font-weight-normal">${data.symbol}</th>
+                        <td>${data.date}</td>
+                        <td>${data.price.toFixed(2)}</td>
+                        <td>${data.qty}</td>
+                        <td><kbd class="bg-light ${(data.type == 'BUY' ? 'text-danger' : 'text-success')}">${data.type}</kbd></td>
+                        <td>${(data.qty * data.price).toFixed(2)}</td>
                     </tr>
                 </tbody>
             `
     })
 
-
-
+    console.log(boughtSymbols)
+    console.log(soldSymbols)
 
 }
